1b0801fba2ab6288488875ba41b7eb7b0105a4c3
Ashutosh Kumar Rout <129250384+routAshutoshhh@users.noreply.github.com>
2024-10-29 15:06:58 +0530

    learning backend by making a newsletter signup , using node and express

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+///sending the html file for the sign up
+//we going to send the css and js files  as they are static in nature so for me being able to send ths files using exress i need to do it using static
+app.use(express.static("public")); // here public is a foldeer inside of which i am going to send all the local files
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var email = req.body.email;
+  var name = req.body.fname;
+});
+
+app.listen(3000, function () {
+  console.log("server running in 3000");
+});
